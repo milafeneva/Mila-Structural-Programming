11522b7ce6ca47ae3005f671a8c70968a7abef58
Mila Feneva <150823289+milafeneva@users.noreply.github.com>
2024-10-24 18:50:03 +0200

    Create 1.h

---
+++ K1/1.h
@@ -0,0 +1,35 @@
+Од стандарден влез се читаат знаци се додека не се прочита извичник. Во вака внесениот текст се скриени цели броеви (помали од 100). 
+Да се напише програма што ќе ги прочита сите знаци и на излез ќе го испечати збирот на сите броеви скриени во текстот.
+
+Напомена: cin во C++ ги игнорира празните места (space). За да не ги игнорира потребно е да се додаде noskipws пред читањето на знакот (пр. cin >> noskipws >> znak;)
+
+#include <iostream>
+using namespace std;
+
+bool isDigit(char current) {
+    for (int i = 0; i <= 10; i++)
+        if (current == (char)('0' + i))
+            return true;
+    return false;
+}
+
+int main() {
+    char current = '0';
+    int zbir = 0;
+    int n = 0;
+
+    while (current != '!') {
+        cin >> current;
+
+        if (isDigit(current)) {
+            n *= 10;
+            n += (int)current - '0';
+        } else {
+            zbir += n;
+            n = 0;
+        }
+    }
+
+    cout << zbir;
+    return 0;
+}
